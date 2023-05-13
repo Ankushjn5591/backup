a29ef3a4584d02b19f981b6f1823d779a5cd64fc
Ankush Jain <ankushjn38@gmaial.com>
2023-05-13 18:23:59 +0530

    only vault

--- Main.tf
+++ Main.tf
@@ -7,10 +7,6 @@ data "azurerm_resource_group" "rg1" {
   name     = "ankushrg"
 }
 
-data "azurerm_virtual_machine" "vm" {
-  name     = "vm1"
-  resource_group_name = "ankushrg"
-}
 
 # Define the Recovery Services Vault
 resource "azurerm_recovery_services_vault" "vault" {
@@ -18,26 +14,6 @@ resource "azurerm_recovery_services_vault" "vault" {
   location            = data.azurerm_resource_group.rg1.location
   resource_group_name = data.azurerm_resource_group.rg1.name
   sku                 = "Standard"
-}
-
-
-
-# Define a backup policy for the virtual machine
-resource "azurerm_recovery_services_protection_policy" "policy" {
-  name                = "vm1policy"
-  resource_group_name = data.azurerm_resource_group.rg1.name
-  recovery_vault_name = azurerm_recovery_services_vault.vault.name
-
-  backup_policy {
-    frequency = "daily"
-    time      = "18:25"
-    retention_daily {
-      count = 5
-    }
-    retention_weekly {
-      count = 4
-    }
-  }
 }
 
 
